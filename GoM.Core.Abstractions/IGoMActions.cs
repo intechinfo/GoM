b0de1556881f6eb97c5e6e109cef70b51d6a0196
Olivier Spinelli <olivier.spinelli@invenietis.com>
2017-05-12 17:39:40 +0200

    First commit.

---
+++ GoM.Core.Abstractions/IGoMActions.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GoM.Core
+{
+    public interface IGoMActions
+    {
+        IGomContext Refresh(IGomContext c);
+
+    }
+}
---
+++ GoM.Core.Abstractions/IProject.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.FileProviders;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GoM.Core
+{
+    public interface IProject
+    {
+        string Path { get; }
+
+        IReadOnlyCollection<ITarget> Targets { get; }
+    }
+}
