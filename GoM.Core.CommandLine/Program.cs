1244fbdb17d3eeff1f320f596b2d4549cc635553
fcuchot <cuchot@intechinfo.fr>
2017-05-15 14:40:59 +0200

    Getting started commandLine

--- GoM.Core.CommandLine/Program.cs
+++ GoM.Core.CommandLine/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Extensions.CommandLineUtils;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,7 +11,44 @@ namespace GoM.Core.CommandLine
     {
         static void Main(string[] args)
         {
+            if(args == null || args.Length == 0)
+            {
+                Console.WriteLine("No arguments");
+            }
+            CommandLineApplication cmdLineApplication = new CommandLineApplication(false);
 
+            cmdLineApplication.Command("Hello", (cmd) =>
+            {
+                cmd.Description = "Simple Hello World";
+                cmd.HelpOption("-h");
+
+                cmd.OnExecute(() =>
+                {
+                    Console.WriteLine("Hello World!");
+                    return 0;
+                });
+
+            }, false);
+
+            cmdLineApplication.Command("GoodBye", (cmd) =>
+            {
+                cmd.Description = "Simple GoodBye";
+                cmd.HelpOption("-h");
+
+                cmd.OnExecute(() =>
+                {
+                    Console.WriteLine("Goodbye!");
+                    return 0;
+                });
+
+            }, false);
+            foreach (var cnt in args)
+            {
+                cmdLineApplication.Execute(cnt);
+            }
+            
+            Console.ReadLine();
+           
         }
-    }
-}
+        }
+}   
