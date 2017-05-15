bdbd313ccb988c1e983ee6e543930fc982116e51
KevinRoussel <roussel@intechinfo.fr>
2017-05-15 15:25:46 +0200

    Begin XML export

--- GoM.Core.Persistence/Persistence.cs
+++ GoM.Core.Persistence/Persistence.cs
@@ -1,9 +1,33 @@
 ﻿using GoM.Core;
+using GoM.Core.Mutable;
 using System;
 using System.IO;
+using System.Xml.Linq;
 
 namespace GoM.Persistence
 {
+    public static class Helper
+    {
+        public static XElement ToXML ( this PackageInstance _this )
+        {
+            XElement element = new XElement(typeof(PackageInstance).Name);
+            element.SetAttributeValue( nameof( _this.Version ), _this.Version );
+            element.SetAttributeValue( nameof( _this.Name ), _this.Name );
+            return element;
+        }
+
+        public static XElement ToXML(this PackageFeed _this)
+        {
+            XElement element = new XElement(typeof(PackageFeed).Name);
+            element.SetAttributeValue( nameof( _this.Url ), _this.Url );
+            foreach ( PackageInstance package in _this.Packages )
+            {
+                element.Add( package.ToXML() );
+            }
+            return element;
+        }
+
+    }
     class Persistence : IPersistence
     {
         string FolderName { get; }
@@ -26,6 +50,13 @@ namespace GoM.Persistence
             if ( ctx == null ) throw new ArgumentNullException();
 
         }
+
+
+        #region Extensions
+
+        
+
+        #endregion
 
     }
 }
--- Tests/GoM.Core.Persistence.Tests/PersistenceTests.cs
+++ Tests/GoM.Core.Persistence.Tests/PersistenceTests.cs
@@ -1,5 +1,8 @@
 using GoM.Core.Mutable;
+using GoM.Persistence;
 using System;
+using System.Collections.Generic;
+using System.Xml.Linq;
 using Xunit;
 
 namespace GoM.Core.Persistence.Tests
@@ -13,10 +16,83 @@ namespace GoM.Core.Persistence.Tests
         }
 
         [Fact]
-        public void t()
+        public void first_xml_try_on_package_instance()
         {
-            IPackageInstance t = new PackageInstance();
-            Assert.True( true );
+
+            PackageInstance pi = new PackageInstance();
+            pi.Name = "SDL";
+            pi.Version = "2.0";
+
+            XDocument doc = new XDocument();
+            doc.Add();
+
+            XElement element = new XElement(typeof(PackageInstance).Name);
+            element.SetAttributeValue( nameof( pi.Version), pi.Version);
+            element.SetAttributeValue( nameof(pi.Name), pi.Name );
+            doc.Add( element );
+
+            var s = doc.ToString();
+            Console.WriteLine( s );
+            Assert.True( s == "<PackageInstance Version=\"2.0\" Name=\"SDL\" />" );
+        }
+
+        [Fact]
+        public void xml_works_using_extensions()
+        {
+            PackageInstance pi = new PackageInstance() { Name = "SDL", Version="2.0" };
+            PackageInstance pi2 = new PackageInstance() { Name = "SDLbit", Version="2.0" };
+            PackageInstance pi3 = new PackageInstance() { Name = "SDLou", Version="2.0" };
+
+            PackageFeed pf = new PackageFeed()
+            {
+                Url = new Uri("http://www.google.fr")
+            };
+            pf.Packages.Add( pi );
+            pf.Packages.Add( pi2 );
+            pf.Packages.Add( pi3 );
+
+
+            XDocument doc = new XDocument();
+            doc.Add( pf.ToXML() );
+
+
+            var s = doc.ToString();
+            Console.WriteLine( s );
+
+        }
+
+
+        [Fact]
+        public void first_xml_try_create_multiple_node ()
+        {
+            PackageInstance pi = new PackageInstance() { Name = "SDL", Version="2.0" };
+            PackageInstance pi2 = new PackageInstance() { Name = "SDLbit", Version="2.0" };
+            PackageInstance pi3 = new PackageInstance() { Name = "SDLou", Version="2.0" };
+
+            PackageFeed pf = new PackageFeed()
+            {
+                Url = new Uri("http://www.google.fr")
+            };
+            pf.Packages.Add( pi );
+            pf.Packages.Add( pi2 );
+            pf.Packages.Add( pi3 );
+
+
+            XElement element = new XElement(typeof(PackageFeed).Name);
+            element.SetAttributeValue( nameof( pf.Url ), pf.Url );
+            foreach(PackageInstance package in pf.Packages)
+            {
+                element.Add( package.ToXML() );
+            }
+
+
+            XDocument doc = new XDocument();
+            doc.Add(element);
+
+
+            var s = doc.ToString();
+            Console.WriteLine( s );
+
         }
 
 
