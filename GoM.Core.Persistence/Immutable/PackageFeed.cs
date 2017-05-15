51971d2b06cc82aa00dd491ca1c559c10c0e93d6
Aymeric Richard <richard.aymeric.92@gmail.com>
2017-05-15 17:52:49 +0200

    Add constructors from XElement on PackageFeed and PackageInstance

--- GoM.Core.Persistence/Immutable/PackageFeed.cs
+++ GoM.Core.Persistence/Immutable/PackageFeed.cs
@@ -1,11 +1,23 @@
 ﻿using GoM.Core;
 using System;
 using System.Collections.Generic;
+using System.Xml.Linq;
 
 namespace GoM.Core.Persistence
 {
     public class PackageFeed : IPackageFeed
     {
+
+        public PackageFeed(XElement e)
+        {
+            this.Url = new Uri(e.Attribute(nameof(Uri)).Value);
+            this.Packages = new List<PackageInstance>();
+            foreach (var t in e.Elements(nameof(PackageInstance)))
+            {
+                this.Packages.Add(new PackageInstance(t));
+            }
+        }
+
         public Uri Url { get; set; }
 
         public List<PackageInstance> Packages { get; } = new List<PackageInstance>();
--- GoM.Core.Persistence/Immutable/PackageInstance.cs
+++ GoM.Core.Persistence/Immutable/PackageInstance.cs
@@ -1,10 +1,17 @@
 ﻿using GoM.Core;
 using System;
+using System.Xml.Linq;
 
 namespace GoM.Core.Persistence
 {
     public class PackageInstance : IPackageInstance
     {
+        public PackageInstance(XElement e)
+        {
+            this.Name = e.Attribute(nameof(Name)).Value;
+            this.Version = e.Attribute(nameof(Version)).Value;
+        }
+
         public string Name { get; set; }
 
         public string Version { get; set; }
