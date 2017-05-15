7a426aa7c47ef9c7d5008fc435e76bc0ccda14f4
dopeldead <gidon@intechinfo.fr>
2017-05-15 11:28:22 +0200

    Added FeedReader project and implemented first methods of IFeedReader

---
+++ GoM.Feeds/IFeedReader.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using GoM.Core;
+namespace GoM.Feeds
+{
+    public interface IFeedReader
+    { 
+        IPackageInstance GetLatestVersion(IPackageInstance package);
+        IPackageInstance GetSpecificVersion(IPackageInstance package);
+
+        List<IPackageInstance> GetDependencies(IPackageInstance package);
+
+        IPackageInstance Search(string packageName);
+    }
+}
