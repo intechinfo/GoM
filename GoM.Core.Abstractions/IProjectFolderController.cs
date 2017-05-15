55361cc1d0778002bd21552ff768216f6b2abcce
Michat <michael.gungaram@gmail.com>
2017-05-15 12:51:02 +0200

    Add Abstractions, create FSAnalyzer project.

---
+++ GoM.Core.Abstractions/IProjectFolderController.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace GoM.Core.Abstractions
+{
+    public class IProjectFolderController
+    {
+    }
+}
---
+++ GoM.Core.Abstractions/IProjectFolderHandler.cs
@@ -0,0 +1,9 @@
+﻿namespace GoM.Core.Abstractions
+{
+    public interface IProjectFolderHandler
+    {
+        bool Sniff(string path);
+
+        IProject Read(string path);
+    }
+}
