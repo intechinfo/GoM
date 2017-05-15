e36582ea41be0da9c560e0d2ec3b25144f3078b3
Aymeric Richard <richard.aymeric.92@gmail.com>
2017-05-15 12:49:28 +0200

    Move GoM.Core.Persistence.Tests to Tests folder

---
+++ Tests/GoM.Core.Persistence.Tests/PersistenceTests.cs
@@ -0,0 +1,19 @@
+using System;
+using Xunit;
+
+namespace GoM.Core.Persistence.Tests
+{
+    public class PersistenceTests
+    {
+        [Fact]
+        public void test_runner_working()
+        {
+            Assert.True( true );
+        }
+
+
+
+
+
+    }
+}
