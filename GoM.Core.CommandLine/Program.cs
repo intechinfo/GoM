b1e30bede3312d0bacd774bddc9e3ca1c2edeba4
Loic <donne@intechinfo.fr>
2017-05-15 11:59:32 +0200

    Initialization of the CommandLineApplication

--- GoM.Core.CommandLine/Program.cs
+++ GoM.Core.CommandLine/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Extensions.CommandLineUtils;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,9 +9,42 @@ namespace GoM.Core.CommandLine
 {
     class Program
     {
-        static void Main(string[] args)
+        public static void Main(params string[] args)
         {
-
+            // Program.exe <-g|--greeting|-$ <greeting>> [name <fullname>]
+            // [-?|-h|--help] [-u|--uppercase]
+            CommandLineApplication cmd = new CommandLineApplication(throwOnUnexpectedArg: false);
+            CommandArgument names = null;
+            cmd.Command("name",
+              (target) =>
+                names = target.Argument(
+                  "fullname",
+                  "Enter the full name of the person to be greeted.",
+                  multipleValues: true));
+            CommandOption greeting = cmd.Option(
+              "-$|-g |--greeting <greeting>",
+              "The greeting to display. The greeting supports"
+              + " a format string where {fullname} will be "
+              + "substituted with the full name.",
+              CommandOptionType.SingleValue);
+            CommandOption uppercase = cmd.Option(
+              "-u | --uppercase", "Display the greeting in uppercase.",
+              CommandOptionType.NoValue);
+            cmd.HelpOption("-? | -h | --help");
+            cmd.OnExecute(() =>
+            {
+                if (greeting.HasValue())
+                {
+                    Greet(greeting.Value(), names.Values, uppercase.HasValue());
+                }
+                return 0;
+            });
+            cmd.Execute(args);
+        }
+        private static void Greet(
+          string greeting, IEnumerable<string> values, bool useUppercase)
+        {
+            Console.WriteLine(greeting);
         }
     }
 }
