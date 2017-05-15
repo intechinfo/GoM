e5e47a96810b598fcc818a8dbfe361a932899979
Anthaax <guillaume.fimes@laposte.net>
2017-05-15 17:49:25 +0200

    RepositoryWrapper for disposable Repository

---
+++ GoM.GitFileProvider/RepositoryWrapper.cs
@@ -0,0 +1,25 @@
+﻿using LibGit2Sharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GoM.GitFileProvider
+{
+    internal class RepositoryWrapper : IDisposable
+    {
+        private Repository _repo;
+        internal Repository Create(string rootPath)
+        {
+            _repo = new Repository(rootPath);
+            return _repo;
+        }
+
+        public void Dispose()
+        {
+            _repo.Dispose();
+            _repo = null;
+        }
+    }
+}
