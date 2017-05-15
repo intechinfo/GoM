464aea1ac44ed6f9d0d17435a56df5714e8c72bd
KevinRoussel <roussel@intechinfo.fr>
2017-05-15 10:26:57 +0200

    GoM.Persistence project init

---
+++ GOM.Persistence/IPersistance.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GoM.Core;
+
+namespace GoM.Persistence
+{
+    interface IPersistance
+    {
+
+        IGomContext Load ();
+
+
+        void Save (IGomContext ctx);
+
+
+    }
+}
