fe28c982ce7e1fbde0a650c60ed174598879d6e2
KevinRoussel <roussel@intechinfo.fr>
2017-05-15 11:22:10 +0200

    FirstImplement Save

--- GOM.Persistence/Persistence.cs
+++ GOM.Persistence/Persistence.cs
@@ -1,15 +1,22 @@
-﻿using GoM.Persistence;
+﻿using GoM.Core;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using GoM.Core;
+using System.IO;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
 
 namespace GoM.Persistence
 {
     class Persistence : IPersistence
     {
+        string FolderName { get; }
+        string FileName { get; }
+
+        public Persistence(string folderName=".gom", string fileName="context")
+        {
+            FolderName = folderName;
+            FileName = fileName;
+        }
+
 
         public IGomContext Load ()
         {
@@ -20,10 +27,18 @@ namespace GoM.Persistence
         {
             if ( ctx == null ) throw new ArgumentNullException();
 
+            Directory.CreateDirectory( Path.Combine( ctx.RootPath, FolderName ) );
 
+            using ( var stream = File.Create( Path.Combine( ctx.RootPath, FolderName, FileName ) ) )
+            {
+                IFormatter formatter = new BinaryFormatter();
 
-            throw new NotImplementedException();
+                formatter.Serialize( stream, ctx );
+            }
+
         }
+
+
 
 
     }
