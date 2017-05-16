279bbdf09b43cf932978867b1db6d86bfa8e7682
Loic <donne@intechinfo.fr>
2017-05-16 11:13:43 +0200

    Working on the GoM add fixture

--- GoM.Core.CommandLine/Program.cs
+++ GoM.Core.CommandLine/Program.cs
@@ -13,38 +13,48 @@ namespace GoM.Core.CommandLine
         public static void Main(params string[] args)
         {
             var app = new CommandLineApplication(throwOnUnexpectedArg: false);
-            app.Name = "gom";
-            app.HelpOption("-h|--help");
-
-            app.OnExecute(() => {
-                Console.WriteLine("Hello World!");
-                return 0;
-            });
-
-            app.Command("hide", (command) =>
-             {
-                 command.Description = "Ceci est une description.";
-                 command.HelpOption("-?,|-h|--help");
-
-                 command.OnExecute(() =>
-                 {
-                     Console.WriteLine("");
-                     return 0;
-                 });
-             });
+            app.Name = "GoM";
 
             app.Command("add", (command) =>
             {
-                command.Description = "Ceci est une description.";
+                command.Description = "This command allow to the user to add repository and projects to his GoM";
                 command.HelpOption("-h|--help");
 
-                var locationArgument = command.Argument("[location]",
-                                    "Where the ninja should hide.");
+                CommandOption repositoryOption = command.Option("-r|--repository",
+                    "Repository to add to GoM"
+                    ,CommandOptionType.MultipleValue);
+
+                CommandOption projectOption = command.Option("-p|--project",
+                    "Project to add to the repository",
+                    CommandOptionType.MultipleValue);
+
+                CommandOption allProjectOption = command.Option("-p -all|--project -all",
+                   "Add all the projects to the repository",
+                   CommandOptionType.MultipleValue);
+
+                CommandOption branchOption = command.Option("-b|--branch",
+                    "Add a branch to GoM",
+                    CommandOptionType.MultipleValue);
+
+                /*CommandOption excludeBranchOption = command.Option("-e -b| --exclude -branch",
+                    "Exclude a branch to GoM",
+                    CommandOptionType.MultipleValue);
+
+                CommandOption excludeprojectOption = command.Option("-e -p| --exclude -project",
+                   "Exclude a project from a repository",
+                   CommandOptionType.MultipleValue);
+
+                CommandOption excludeAllProjectOption = command.Option("-e -p -all| --exclude -project - all",
+                  "Exclude all projects from the repository",
+                  CommandOptionType.MultipleValue);*/
+
+                CommandArgument projectLocationArgument = command.Argument("[location]","Where the projects should be located");
 
                 command.OnExecute(() =>
                 {
-                    var location = locationArgument.Values.Count() > 0 ? locationArgument.Value : "under a turtle";
-                    Console.WriteLine("Ninja is hidden " + location);
+                    var location = projectLocationArgument.Values.Count() > 0 ? projectLocationArgument.Value : "no value";
+                    //var projectPath = projectLocationArgument.Value != null && projectLocationArgument.Value != "" ? projectLocationArgument.Value : Directory.GetDirectories(location);
+                    Console.WriteLine(location);
                     return 0;
                 });
             });
@@ -52,7 +62,7 @@ namespace GoM.Core.CommandLine
             app.Command("attack", (command) =>
             {
                 command.Description = "Instruct the ninja to go and attack!";
-                command.HelpOption("-?|-h|--help");
+                command.HelpOption("-h|--help");
 
                 var excludeOption = command.Option("-e|--exclude <exclusions>",
                                "Branch/Repository to exclude of the selection.",
@@ -85,6 +95,8 @@ namespace GoM.Core.CommandLine
                     return 0;
                 });
              });
+
+            /// Command list file
             app.Command("files", c =>
             {
 
