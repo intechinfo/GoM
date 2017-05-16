0a759743fd9a6c00fd9c9814a3942e37d7592805
VanPalamar <rami.morri@gmail.com>
2017-05-16 15:37:58 +0200

    GoM: Modif: csproj parser (new and old version) + Package.config Parser

--- GoM.Core.FSAnalyzer/Utils/CsProjParser.cs
+++ GoM.Core.FSAnalyzer/Utils/CsProjParser.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Text;
 using Microsoft.Extensions.FileProviders;
+using System.Xml.Linq;
+using System.Linq;
+using GoM.Core.Mutable;
 
 namespace GoM.Core.FSAnalyzer.Utils
 {
@@ -13,7 +15,57 @@ namespace GoM.Core.FSAnalyzer.Utils
 
         public override IEnumerable<ITarget> Read()
         {
-            throw new NotImplementedException();
+            List<ITarget> target = null;
+            var physicalPath = Source.PhysicalPath;
+            XDocument xmldoc = XDocument.Load(physicalPath);
+            XNamespace msbuild = xmldoc.Root.GetDefaultNamespace().NamespaceName;
+            var ReferenceList = xmldoc.Descendants(msbuild + "Reference").ToArray();
+            var ReferenceListOld = xmldoc.Descendants(msbuild + "PackageReference").ToArray();
+            var ta = new Target();
+
+            var TargetFrameworkVersionList = xmldoc.Descendants(msbuild + "TargetFrameworkVersion").ToArray();
+
+            for (int i = 0; i < TargetFrameworkVersionList.Length; i++)
+            {
+                string targetFrameworkVersion = TargetFrameworkVersionList[i] != null ? TargetFrameworkVersionList[i].Value : string.Empty;
+
+                ta = new Target()
+                {
+                    Name = targetFrameworkVersion,
+
+                };
+
+                target.Add(ta);
+            }
+
+            if (ReferenceList.Length > 0)
+            {
+                for (int i = 0; i < ReferenceList.Length; i++)
+                {
+                    string include = ReferenceList[i] != null ? ReferenceList[i].Attribute("Include").Value : string.Empty;
+
+                    ta.Dependencies.Add(new TargetDependency()
+                    {
+                        Name = "",
+                        Version = include
+                    });
+                }
+            }
+            else
+            {
+                for (int i = 0; i < ReferenceListOld.Length; i++)
+                {
+                    string include = ReferenceListOld[i] != null ? ReferenceListOld[i].Attribute("Include").Value : string.Empty;
+                    string version = ReferenceListOld[i] != null ? ReferenceListOld[i].Element("Version").Value : string.Empty;
+                    ta.Dependencies.Add(new TargetDependency()
+                    {
+                        Name = include,
+                        Version = version
+                    });
+                }
+            }
+            
+            return target;
         }
     }
 }
---
+++ GoM.Core.FSAnalyzer/Utils/PackageConfigParser.cs
@@ -0,0 +1,51 @@
+﻿using GoM.Core.Mutable;
+using Microsoft.Extensions.FileProviders;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Xml.Linq;
+
+namespace GoM.Core.FSAnalyzer.Utils
+{
+    class PackageConfigParser : BaseConfigParser
+    {
+        public PackageConfigParser(IFileInfo file) : base(file)
+        {
+        }
+
+        public override IEnumerable<ITarget> Read()
+        {
+            List<ITarget> target = null;
+            var physicalPath = Source.PhysicalPath;
+            XDocument xml = new XDocument();
+            FileStream xmlStream = new FileStream(physicalPath, FileMode.Open);
+            var result = XDocument.Load(xmlStream);
+            var pac = (from c in result.Root.Elements("package")
+                       select c).ToArray();
+            var ta = new Target();
+
+            for (var i = 0; i < pac.Count(); i++)
+            {
+                var id = pac[i].Attribute("id") != null ? pac[i].Attribute("id").Value : "null";
+                var version = pac[i].Attribute("version") != null ? pac[i].Attribute("version").Value : "null";
+                var targetFramework = pac[i].Attribute("targetFramework") != null ? pac[i].Attribute("targetFramework").Value : "null";
+
+                ta = new Target()
+                {
+                    Name = id
+                };
+                ta.Dependencies.Add(new TargetDependency()
+                {
+                    Name = targetFramework,
+                    Version = version
+                });
+                target.Add(ta);
+
+
+            }
+            return null;
+        }
+    }
+}
