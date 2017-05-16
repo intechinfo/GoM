6a74291f1e19cdc55f54583a62fe05991c91bb44
Pawus <pawus13@gmail.com>
2017-05-16 16:10:35 +0200

    Intégration de Extract.Tests

---
+++ Tests/GoM.Core.Extract.Tests/ExctractTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Xunit;
+using FluentAssertions;
+
+namespace GoM.Core.Extract.Tests
+{
+    class ExctractTests
+    {
+
+        private String pathTest = @"C:\Users\Nicolas\Test_Git2sharp";
+
+        [Fact]
+        public void folderIsGitRepository()
+        {
+
+            ReadProject rp = new ReadProject(this.pathTest);
+            rp.gitRepositoryExist().Should().Be(true);
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
