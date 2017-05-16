fe36295c955c987346630f1b178423f4e0a4c6cc
VanPalamar <rami.morri@gmail.com>
2017-05-16 16:58:04 +0200

    Gom: Modif: Test unitaires (csprojParser + packageConfigParser)

---
+++ Tests/GoM.Core.FsAnalyzer.Tests/CsProjParserTests.cs
@@ -0,0 +1,17 @@
+﻿using Xunit;
+using GoM.Core.FSAnalyzer.Utils;
+using Microsoft.Extensions.FileProviders.Physical;
+
+namespace GoM.Core.FsAnalyzer.Tests
+{
+    public class CsProjParserTests
+    {
+        [Fact]
+        public void CsProjParser()
+        {
+            var p = new PhysicalFileInfo(new System.IO.FileInfo("call your own path to test"));
+            var t = new CsProjParser(p);
+            var k = t.Read();
+        }
+    }
+}
---
+++ Tests/GoM.Core.FsAnalyzer.Tests/PackageConfigParserTests.cs
@@ -0,0 +1,17 @@
+﻿using Xunit;
+using GoM.Core.FSAnalyzer.Utils;
+using Microsoft.Extensions.FileProviders.Physical;
+
+namespace GoM.Core.FsAnalyzer.Tests
+{
+    public class PackageConfigParserTests
+    {
+        [Fact]
+        public void PackageConfigParser() { 
+            var p = new PhysicalFileInfo(new System.IO.FileInfo("call your own path to test"));
+            var t = new PackageConfigParser(p);
+            var k = t.Read();
+        }
+    }
+}
+
