a4c3edec571eb0f0a9d91c2b885044271b8351fa
KevinRoussel <roussel@intechinfo.fr>
2017-05-16 11:03:29 +0200

    Add ful fake context

--- Tests/GoM.Core.Persistence.Tests/PersistenceTests.cs
+++ Tests/GoM.Core.Persistence.Tests/PersistenceTests.cs
@@ -102,12 +102,136 @@ namespace GoM.Core.Persistence.Tests
             IGoMContext context = new GoMContext(doc.Root);
         }
 
-        public IGoMContext GenerateFakeContextHelper()
+        [Fact]
+        public void test_generatefake()
         {
 
+            var ctx = GenerateFakeContextHelper();
 
+            Console.Write( ctx );
 
+        }
 
+        public IGoMContext GenerateFakeContextHelper()
+        {
+            // OK
+            var package1 = new Mutable.PackageInstance();
+            package1.Version = "";
+            package1.Name = "";
+
+            // OK
+            var package2 = new Mutable.PackageInstance();
+            package2.Version = "";
+            package2.Name = "";
+
+            // OK
+            var package3 = new Mutable.PackageInstance();
+            package3.Version = "";
+            package3.Name = "";
+
+            // OK
+            var feed1 = new Mutable.PackageFeed();
+            feed1.Url = new Uri( "http://www.google.fr" );
+            feed1.Packages.Add( package1 );
+            feed1.Packages.Add( package2 );
+            feed1.Packages.Add( package3 );
+
+            // OK
+            var feed2 = new Mutable.PackageFeed();
+            feed2.Url = new Uri( "http://www.google.fr" );
+
+            // OK
+            var targetDepency = new Mutable.TargetDependency();
+            targetDepency.Name = "";
+            targetDepency.Version = "";
+
+            // OK
+            var target1 = new Mutable.Target();
+            target1.Name = "";
+            target1.Dependencies.Add( targetDepency );
+
+            // OK
+            var target2 = new Mutable.Target();
+            target2.Name = "";
+            
+            // OK
+            var target3 = new Mutable.Target();
+            target2.Name = "";
+
+            // OK
+            var project = new Mutable.Project();
+            project.Path = "";
+            project.Targets.Add(target1);
+            project.Targets.Add(target2);
+
+            // OK
+            var project2 = new Mutable.Project();
+            project.Path = "";
+            project.Targets.Add( target3 );
+
+            // OK
+            var project3 = new Mutable.Project();
+            project.Path = "";
+
+            // OK
+            var versionTag = new Mutable.VersionTag();
+            versionTag.FullName = "";
+            
+            // OK
+            var branchVersion = new Mutable.BranchVersionInfo();
+            branchVersion.LastTag = versionTag;
+            branchVersion.LastTagDepth = 0;
+
+            // OK
+            var realGitbranch = new Mutable.GitBranch();
+            realGitbranch.Details = realGitbranch;
+            realGitbranch.Name = "";
+
+            realGitbranch.Version = branchVersion;
+
+            realGitbranch.Projects.Add(project);
+            realGitbranch.Projects.Add(project2);
+            realGitbranch.Projects.Add(project3);
+
+            // OK
+            var basicbranch = new Mutable.BasicGitBranch();
+            basicbranch.Details = realGitbranch;
+            basicbranch.Name = "";
+
+            // OK
+            var basicbranch2 = new Mutable.BasicGitBranch();
+            basicbranch2.Details = null;
+            basicbranch2.Name = "";
+
+            // OK
+            var realgitrepo = new Mutable.GitRepository();
+            realgitrepo.Url = new Uri( "http://www.google.fr" );
+            realgitrepo.Path = "";
+            realgitrepo.Details = realgitrepo;
+            realgitrepo.Branches.Add( basicbranch);
+            realgitrepo.Branches.Add( basicbranch2 );
+
+            // OK
+            var basicrepo2 = new Mutable.BasicGitRepository();
+            basicrepo2.Details = realgitrepo;
+            basicrepo2.Path = "";
+            basicrepo2.Url = new Uri( "http://www.google.fr" );
+
+            // OK
+            var basicrepo = new Mutable.BasicGitRepository();
+            basicrepo.Path = "";
+            basicrepo.Url = new Uri( "http://www.google.fr" );
+            basicbranch.Details = null;
+
+            // OK
+            Mutable.GoMContext ctx = new Mutable.GoMContext();
+            ctx.RootPath = "";
+            ctx.Feeds.Add( feed1 );
+            ctx.Feeds.Add( feed2 );
+            ctx.Repositories.Add( basicrepo2 );
+            ctx.Repositories.Add( basicrepo );
+
+            return ctx;
         }
 
 
