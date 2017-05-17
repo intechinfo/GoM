c7e48409dbcf60559498a86fce17da760d49d024
dopeldead <gidon@intechinfo.fr>
2017-05-17 11:45:23 +0200

    Snif done

--- GoM.Feeds/Abstractions/PypiFeedReader.cs
+++ GoM.Feeds/Abstractions/PypiFeedReader.cs
@@ -6,8 +6,9 @@ using GoM.Core;
 
 namespace GoM.Feeds.Abstractions
 {
-    abstract class PypiFeedReader : IFeedReader
+    public abstract class PypiFeedReader : IFeedReader
     {
+        public abstract Task<bool> FeedMatch(Uri adress);
         public abstract Task<IEnumerable<IPackageInstance>> GetAllVersions(string name);
         public abstract Task<IEnumerable<ITarget>> GetDependencies(string name, string version);
         public abstract Task<IEnumerable<IPackageInstance>> GetNewestVersions(string name, string version);
--- GoM.Feeds/NpmJsFactory.cs
+++ GoM.Feeds/NpmJsFactory.cs
@@ -3,24 +3,31 @@ using System;
 using System.Collections.Generic;
 using System.Text;
 using GoM.Core;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace GoM.Feeds
 {
-    internal class NpmJsFactory : IFeedFactory
+    public class NpmJsFactory : IFeedFactory
     {
-        public IFeedReader GetFeedReader()
+        NpmJsFeedReader _feedReader;
+        public NpmJsFactory()
         {
-            throw new NotImplementedException();
+            _feedReader = new NpmJsFeedReader();
         }
+        public IEnumerable<IFeedReader> FeedReaders => new List<IFeedReader> { _feedReader };
 
         public IEnumerable<IFeedReader> Snif(List<Uri> links)
         {
-            throw new NotImplementedException();
+            return links.SelectMany(x => Snif(x));
         }
 
-        public IFeedReader Snif(Uri link)
+        public IEnumerable<IFeedReader> Snif(Uri link)
         {
-            throw new NotImplementedException();
+            var list = new List<IFeedReader>();
+            var fr= _feedReader.FeedMatch(link).Result  ? _feedReader : null;
+            if (fr != null) list.Add(fr);
+            return list;
         }
     }
 }
