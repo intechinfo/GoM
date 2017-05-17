1b65844972f1970815b624292b9faf903a73207c
Anthaax <guillaume.fimes@laposte.net>
2017-05-17 12:37:50 +0200

    GitFilesProvierDirectoryTest

--- GoM.GitFileProvider.Tests/GitFilesProvierDirectoryTest.cs
+++ GoM.GitFileProvider.Tests/GitFilesProvierDirectoryTest.cs
@@ -28,5 +28,36 @@ namespace GoM.GitFileProvider.Tests
                     item.PhysicalPath.Should().Be(ProjectRootPath + Path.DirectorySeparatorChar + item.Name);
             }
         }
+
+        [Test]
+        public void Get_all_branches_of_the_repository()
+        {
+            GitFileProvider git = new GitFileProvider(ProjectRootPath);
+            var rootDir = git.GetDirectoryContents(@"branches");
+
+            rootDir.Exists.Should().BeTrue();
+            foreach (var item in rootDir)
+            {
+                var branchDir =  git.GetDirectoryContents(item.PhysicalPath + @"GoM.Core.Abstractions");
+                branchDir.Exists.Should().BeTrue();
+                foreach (var dir in branchDir)
+                {
+                    dir.Exists.Should().BeTrue();
+                    if (dir.IsDirectory)
+                    {
+                        dir.PhysicalPath.Should().Be(ProjectRootPath + Path.DirectorySeparatorChar + item.Name + Path.DirectorySeparatorChar);
+
+                    }
+                    else if(dir.Name.Contains("cs"))
+                    {
+                        dir.Length.Should().BeGreaterThan(0);
+                    }
+                    else
+                        dir.PhysicalPath.Should().Be(ProjectRootPath + Path.DirectorySeparatorChar + item.Name);
+                }
+            }
+        }
+
+
     }
 }
