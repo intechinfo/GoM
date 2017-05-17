0b75275e22b09a283dffc239ee304abfa346210a
Michat <michael.gungaram@gmail.com>
2017-05-17 10:06:29 +0200

    Add overrided methods for PhpProjectHandler.

--- GoM.Core.FSAnalyzer/PhpProjectHandler.cs
+++ GoM.Core.FSAnalyzer/PhpProjectHandler.cs
@@ -1,10 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Microsoft.Extensions.FileProviders;
+using GoM.Core.Abstractions;
 
 namespace GoM.Core.FSAnalyzer
 {
-    class PhpProjectHandler
+    public class PhpProjectHandler: BaseProjectFolderHandler
     {
+        public PhpProjectHandler(IFileProvider provider) 
+            : base( provider )
+        {
+        }
+
+        public override IProjectFolderHandler Sniff()
+        {
+            return this;
+        }
+
+        public override IProject Read()
+        {
+            return base.Read();
+        }
     }
 }
