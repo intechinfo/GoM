5e0529130ab42281412902f1bf94ec4d2d33cde9
Damien Escande <damien.escande@sharingcloud.com>
2017-05-18 15:44:56 +0200

    WIP PypiOrgFeedReader GetDependencies

--- GoM.Feeds/PypiOrgFeedReader.cs
+++ GoM.Feeds/PypiOrgFeedReader.cs
@@ -1,6 +1,7 @@
 using GoM.Core;
 using GoM.Core.Mutable;
 using GoM.Feeds.Abstractions;
+using GoM.Feeds.Results;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using Semver;
@@ -16,77 +17,61 @@ namespace GoM.Feeds
 {
     public class PypiOrgFeedReader : FeedReaderBase
     {
-        string _baseUrl = "http://registry.npmjs.org/";
+        string _baseUrl = "https://pypi.python.org/pypi/Python/json";
 
-        public async override Task<bool> FeedMatch(Uri adress)
+        public override async Task<FeedMatchResult> FeedMatch(Uri adress)
         {
-            HttpResponseMessage response = await HttpClient.GetAsync(adress);
+            if (String.IsNullOrWhiteSpace(adress.OriginalString)) throw new ArgumentNullException("The Uril adress cannot be null or Empty");
 
-            if (!response.IsSuccessStatusCode)
+            var result = await GetJson(adress);
+            if (result.Success)
             {
-                if (response.StatusCode == HttpStatusCode.NotFound)
+                JObject o = result.Result;
+
+                if (!o.HasValues) throw new InvalidOperationException("No data found from " + adress.ToString() + " .");
+                bool isPypi = o.TryGetValue("info", out JToken value);
+                if (isPypi)
                 {
-                    throw new ArgumentException("Package could not be found");
+                    return new FeedMatchResult(null,o.Property("info").Value.ToString() == "registry");
                 }
-                throw new InvalidOperationException("an error occured while request server status code:" + response.StatusCode);
+                return new FeedMatchResult(null, false);
             }
-            string resp = await response.Content.ReadAsStringAsync();
-
-            JObject o;
-            try
-            {
-                o = JObject.Parse(resp);
-            }
-            catch (JsonReaderException)
-            {
-                return false;
-            }
-            if (!o.HasValues) throw new InvalidOperationException("No data found from " + adress.ToString() + " .");
-            bool isPypi = o.TryGetValue("info", out JToken value);
-            if (isPypi)
-            {
-                var datas = new JObject(o.Property("info"));
-                return datas["name"].Value<string>() == "Python";
-            }
-            return false;
+            return new FeedMatchResult(result.NetworkException ?? result.JsonException, false);
         }
 
-        public override async Task<IEnumerable<IPackageInstance>> GetAllVersions(string name)
+
+        public override async Task<GetPackagesResult> GetAllVersions(string name)
         {
             if (string.IsNullOrWhiteSpace(name)) throw new ArgumentException("The parameter name cannot be null or empty.");
             name = name.ToLowerInvariant();
 
-            HttpResponseMessage response = await HttpClient.GetAsync(_baseUrl + name + "/json");
-
-            if (!response.IsSuccessStatusCode)
+            var result = await GetJson(new Uri( _baseUrl + name));
+            if (result.Success)
             {
-                if (response.StatusCode == HttpStatusCode.NotFound)
+                JObject o = result.Result;
+                if (!o.HasValues )
                 {
-                    throw new ArgumentException("Package could not be found");
+                    return new GetPackagesResult(new InvalidOperationException("No package named : " + name + " found."),null);
                 }
-                throw new InvalidOperationException("an error occured while request server status code:" + response.StatusCode);
-            }
-            string resp = await response.Content.ReadAsStringAsync();
-
-            JObject o = JObject.Parse(resp);
-            if (!o.HasValues) throw new InvalidOperationException("No package named : " + name + " found.");
-
-            var list = new List<IPackageInstance>();
-            var versions = new JObject( new JObject(o["info"]).Property("releases"));
-            foreach (var item in versions)
-            {
-
-                if (SemVersion.TryParse(item.Key, out SemVersion item_v))
+                var list = new List<IPackageInstance>();
+                var versions = new JObject( new JObject(o["info"]).Property("releases"));
+                foreach (var item in versions)
                 {
-                    string packageVersion = item.Key;
-                    string packageName = o["info"].Value<string>("name");
-                    list.Add(new PackageInstance { Name = packageName, Version = packageVersion });
+
+                    if (SemVersion.TryParse(item.Key, out SemVersion item_v))
+                    {
+                        string packageVersion = item.Key;
+                        string packageName = o["info"].Value<string>("name");
+                        list.Add(new PackageInstance { Name = packageName, Version = packageVersion });
+                    }
                 }
+                return new GetPackagesResult(null, list);
+                
             }
-            return list;
+
         }
 
-        public override async Task<IEnumerable<ITarget>> GetDependencies(string name, string version)
+        public override async Task<GetDependenciesResult> GetDependencies(string name, string version)
         {
             if (string.IsNullOrWhiteSpace(name)) throw new ArgumentException("The parameter name cannot be null or empty.");
             if (string.IsNullOrWhiteSpace(version)) throw new ArgumentException("The parameter version cannot be null or empty.");
