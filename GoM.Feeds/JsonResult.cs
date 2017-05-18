82ec41761448a2a6701538c663b2f1d88f1a9c80
Olivier Spinelli <olivier.spinelli@invenietis.com>
2017-05-18 12:11:29 +0200

    Added FeedReaderBase.

---
+++ GoM.Feeds/JsonResult.cs
@@ -0,0 +1,31 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Text;
+
+namespace GoM.Feeds
+{
+    public class JsonResult
+    {
+        public readonly JObject Result;
+
+        public readonly Exception NetworkException;
+
+        public HttpStatusCode? StatusCode;
+
+        public readonly JsonReaderException JsonException;
+
+        public bool Success => Result != null;
+
+        public JsonResult(Exception nE, HttpStatusCode? c, JsonReaderException rE, JObject r )
+        {
+            Result = r;
+            NetworkException = nE;
+            StatusCode = c;
+            Result = r;
+        }
+
+    }
+}
