4cb6ea4bdafe04f180ebe91dfdd40b9b9fe1f75b
KevinRoussel <roussel@intechinfo.fr>
2017-05-18 14:13:28 +0200

    remove some useless methodes

--- Tests/GoM.Core.Persistence.Tests/PersistenceTests.cs
+++ Tests/GoM.Core.Persistence.Tests/PersistenceTests.cs
@@ -23,25 +23,6 @@ namespace GoM.Core.Persistence.Tests
         }
 
         [Fact]
-        public void first_xml_try_on_package_instance()
-        {
-
-            PackageInstance pi = new PackageInstance("SDL", "2.0");
-
-            XDocument doc = new XDocument();
-            doc.Add();
-
-            XElement element = new XElement(typeof(PackageInstance).Name);
-            element.SetAttributeValue(nameof(pi.Version), pi.Version);
-            element.SetAttributeValue(nameof(pi.Name), pi.Name);
-            doc.Add(element);
-
-            var s = doc.ToString();
-            Console.WriteLine(s);
-            Assert.True(s == "<PackageInstance Version=\"2.0\" Name=\"SDL\" />");
-        }
-
-        [Fact]
         public void xml_works_using_extensions()
         {
             PackageInstance pi = new PackageInstance("SDL", "2.0");
@@ -61,51 +42,6 @@ namespace GoM.Core.Persistence.Tests
             var s = doc.ToString();
             Console.WriteLine(s);
 
-        }
-
-
-        [Fact]
-        public void first_xml_try_create_multiple_node()
-        {
-            PackageInstance pi = new PackageInstance("SDL", "2.0");
-            PackageInstance pi2 = new PackageInstance("SDL2", "2.02");
-            PackageInstance pi3 = new PackageInstance("SDL3", "2.03");
-
-            PackageFeed pf = new PackageFeed(new Uri("http://www.google.fr"));
-            pf.Packages.Add(pi);
-            pf.Packages.Add(pi2);
-            pf.Packages.Add(pi3);
-
-
-            XElement element = new XElement(typeof(PackageFeed).Name);
-            element.SetAttributeValue(nameof(pf.Url), pf.Url);
-            foreach (PackageInstance package in pf.Packages)
-            {
-                element.Add(package.ToXML());
-            }
-
-
-            XDocument doc = new XDocument();
-            doc.Add(element);
-
-
-            var s = doc.ToString();
-            Console.WriteLine(s);
-
-        }
-
-        [Fact]
-        public void read_version()
-        {
-            Assert.True(true);
-        }
-
-        [Fact]
-        public void try_read_first_node()
-        {
-            var data = File.ReadAllText(Path.Combine(".", ".gom", "name"));
-            XDocument doc = XDocument.Parse(data);
-            IGoMContext context = new GoMContext(doc.Root);
         }
 
         [Fact]
