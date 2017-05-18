12c14775b209cbc381b38bae04a7b37268d245ac
Aymeric Richard <richard.aymeric.92@gmail.com>
2017-05-18 17:14:53 +0200

    Clean

---
+++ Tests/CreateSampleRoot.ps1
@@ -0,0 +1,38 @@
+md Dev
+cd Dev
+md CK-Core-Projects
+cd CK-Core-Projects
+git clone https://github.com/Invenietis/CK-Text.git CK-Text
+git clone https://github.com/Invenietis/CK-Core.git CK-Core
+git clone https://github.com/Invenietis/CK-ActivityMonitor.git CK-ActivityMonitor
+git clone https://github.com/Invenietis/CK-Monitoring.git CK-Monitoring
+git clone https://github.com/Invenietis/CK-Auth-Abstractions.git CK-Auth-Abstractions
+git clone https://github.com/Invenietis/CK-Reflection.git CK-Reflection
+cd..
+md CK-Database-Projects
+cd CK-Database-Projects
+git clone https://bitbucket.org/olivier_spinelli/ck-database.git CK-Database
+git clone https://github.com/Invenietis/CK-DB.git CK-DB
+git clone https://github.com/Invenietis/CK-Setup-Dependency.git CK-Setup-Dependency
+git clone https://bitbucket.org/olivier_spinelli/ck-sqlserver-parser-model.git CK-SqlServer-Parser-Model
+git clone https://bitbucket.org/olivier_spinelli/ck-sqlserver-parser.git CK-SqlServer-Parser
+cd..
+md CK-AspNet-Projects
+cd CK-AspNet-Projects
+git clone https://github.com/Invenietis/CK-AspNet.git CK-AspNet
+git clone https://github.com/Invenietis/CK-AspNet-Auth.git CK-AspNet-Auth
+cd..
+git clone https://github.com/Invenietis/yodii.git Yodii
+git clone https://github.com/Invenietis/nunit-gui.git NUnit-GUI
+rem Leaving Dev subfolder
+cd..
+
+
+
+
+
+
+
+
+
+
