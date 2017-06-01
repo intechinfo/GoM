abefbb5e1c7456656da39ca25f7c050836ee9d1f
dopeldead <gidon@intechinfo.fr>
2017-06-01 10:56:33 +0200

    hanged comments with new returns types

--- GoM.Feeds/Abstractions/IFeedFactory.cs
+++ GoM.Feeds/Abstractions/IFeedFactory.cs
@@ -9,17 +9,17 @@ namespace GoM.Feeds.Abstractions
     public interface IFeedFactory: IDisposable
     {
         /// <summary>
-        /// Returns all the readers from the inner factories
+        /// Returns all the <see cref="IFeedReader"/> from the inner factories
         /// </summary>
         IEnumerable<IFeedReader> FeedReaders { get; }
         /// <summary>
-        /// Returns the <see cref="IFeedReader"/> that match at least one of the given <see cref="Uri"/>
+        /// Returns the search result <see cref="IFeedReader"/> that match at least one of the given <see cref="Uri"/>
         /// </summary>
         /// <param name="links">A list of <see cref="Uri"/> used as <see cref="IFeedReader"/> identifiers </param>
         /// <returns>A <see cref="IEnumerable{IFeedReader}"/></returns>
         Task<IEnumerable<FeedMatchResult>> Snif(IEnumerable<Uri> links);
         /// <summary>
-        /// Returns the <see cref="IFeedReader"/> that match the given <see cref="Uri"/>
+        /// Returns the search result <see cref="IFeedReader"/> that match the given <see cref="Uri"/>
         /// </summary>
         /// <param name="link">A <see cref="Uri"/> used as <see cref="IFeedReader"/> identifier </param>
         /// <returns>A <see cref="IEnumerable{IFeedReader}"/></returns>
--- GoM.Feeds/Abstractions/IFeedReader.cs
+++ GoM.Feeds/Abstractions/IFeedReader.cs
@@ -13,26 +13,26 @@ namespace GoM.Feeds.Abstractions
         /// Gets all the version of a <see cref="IPackageInstance"/>
         /// </summary>
         /// <param name="name">the name of the package to search</param>
-        /// <returns>A <see cref="Task"/> of all the instances getting retreived from the web</returns>
+        /// <returns>A <see cref="Task{ReadPackagesResult}"/> of all the instances getting retreived from the web</returns>
         Task<ReadPackagesResult> GetAllVersions(string name);
         /// <summary>
         /// Gets the newest version of a <see cref="IPackageInstance"/>
         /// </summary>
         /// <param name="name">the name of the package to search</param>
-        /// <returns>A <see cref="Task"/> of all the instances getting retreived from the web</returns>
+        /// <returns>A <see cref="Task{ReadPackagesResult}"/> of all the instances getting retreived from the web</returns>
         Task<ReadPackagesResult> GetNewestVersions(string name,string version);
         /// <summary>
         /// Gets all the dependencies of a package for  given version and package name
         /// </summary>
         /// <param name="name">the name of the package to search</param>
         /// <param name="version">the version of the package to search</param>
-        /// <returns>A <see cref="Task"/> of all the instances getting retreived from the web</returns>
+        /// <returns>A <see cref=" Task{ReadDependenciesResult}"/> of all the instances getting retreived from the web</returns>
         Task<ReadDependenciesResult> GetDependencies(string name, string version);
         /// <summary>
         /// Finds if a Feed is mathing a given endpoint
         /// </summary>
         /// <param name="adress">The endpoints <see cref="Uri"/> to check the result</param>
-        /// <returns> the pending <see cref="Task"/> of the return</returns>
+        /// <returns> the pending <see cref="Task{FeedMatchResult}"/> of the return</returns>
         Task<FeedMatchResult> FeedMatch(Uri adress);
     }
 }
